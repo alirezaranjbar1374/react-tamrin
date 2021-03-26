0fcb92836d1126c5dfe9927a7b549c8309a31fcb
alirezaranjbar1374 <ali.r.ranjbar1374@gmail.com>
2021-03-26 18:01:06 +0430

    creat-react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App()
+
+{
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          alireza ranjbar :)
+        </p>
+        <h1 > salm</h1>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          alireza
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
